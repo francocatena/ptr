ac1582e69556f7f16f741776fb85b3cf0e5b6374
Franco Catena <francocatena@gmail.com>
2017-08-22 19:14:10 -0300

    Support icons on input helper

--- lib/ptr_web/views/input_helpers.ex
+++ lib/ptr_web/views/input_helpers.ex
@@ -4,37 +4,62 @@ defmodule PtrWeb.InputHelpers do
   import PtrWeb.ErrorHelpers, only: [error_tag: 2]
 
   def input(form, field, label \\ nil, opts \\ []) do
-    {using, opts} = Keyword.pop(opts, :using)
-    type          = using || input_type(form, field)
-    error         = error_tag(form, field)
+    input_html = opts[:input_html] || []
+    label_html = opts[:label_html] || [class: "label"]
+    type       = opts[:using]      || input_type(form, field)
+    icon_opts  = opts[:icons]      || []
+    errors     = error_tag(form, field)
 
     content_tag(:div, class: "field") do
-      label = label_for(form, field, label)
-      input = input(type, form, field, error, opts)
+      label = label_for(form, field, label, label_html)
+      input = input(type, form, field, errors, icon_opts, input_html)
 
-      [label, input, error]
+      [label, input, errors]
     end
   end
 
-  defp label_for(form, field, label) do
-    label(form, field, label || humanize(field), class: "label")
+  defp label_for(form, field, nil, label_html) do
+    label(form, field, humanize(field), label_html)
   end
 
-  defp input_opts(form, field, error, opts) do
-    default_opts = [class: input_class(error)]
-    validations  = input_validations(form, field)
+  defp label_for(form, field, label, label_html) do
+    label(form, field, label, label_html)
+  end
+
+  defp input(type, form, field, errors, icon_opts, input_html) do
+    input_opts   = input_opts(form, field, errors, input_html)
+    input        = apply(Phoenix.HTML.Form, type, [form, field, input_opts])
+    content_opts = [class: wrapper_classes(icon_opts)]
+
+    content_tag(:div, content_opts) do
+      [input | input_icons(icon_opts)]
+    end
+  end
+
+  defp input_opts(form, field, errors, opts) do
+    {class, opts} = Keyword.pop(opts, :class)
+    default_opts  = [class: input_class(class || "input", errors)]
+    validations   = input_validations(form, field)
 
     default_opts
     |> Keyword.merge(validations)
     |> Keyword.merge(opts)
   end
 
-  defp input_class([]), do: "input"
-  defp input_class(_),  do: "input is-danger"
+  defp input_class(default, []), do: default
+  defp input_class(default, _),  do: "#{default} is-danger"
 
-  defp input(type, form, field, error, opts) do
-    input_opts = input_opts(form, field, error, opts)
+  defp input_icons(icon_opts) do
+    for {position, icon} <- icon_opts do
+      content_tag(:span, class: "icon is-#{position}") do
+        content_tag(:i, "", class: "fa fa-#{icon}")
+      end
+    end
+  end
 
-    apply(Phoenix.HTML.Form, type, [form, field, input_opts])
+  defp wrapper_classes(icon_opts) do
+    icon_classes = for {position, _} <- icon_opts, do: "has-icons-#{position}"
+
+    ["control" | icon_classes] |> Enum.join(" ")
   end
 end
--- test/ptr_web/views/input_helpers_test.exs
+++ test/ptr_web/views/input_helpers_test.exs
@@ -13,18 +13,36 @@ defmodule PtrWeb.InputHelpersTest do
           |> safe_to_string()
 
         assert input =~ "Name"
+        assert input =~ "class=\"field\""
+        assert input =~ "class=\"control\""
+        assert input =~ "class=\"input\""
+        assert input =~ "class=\"label\""
+        refute input =~ "has-icons"
         ""
       end)
     end
 
-    test "with custom options", %{conn: conn} do
+    test "with custom input options", %{conn: conn} do
       form_for(conn, "/", [as: :test], fn form ->
         input =
-          input(form, :name, "Test label", class: "test_class")
+          input(form, :name, nil, input_html: [class: "test-input-class"])
+          |> safe_to_string()
+
+        assert input =~ "test-input-class"
+        refute input =~ "input_html"
+        ""
+      end)
+    end
+
+    test "with custom label options", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, "Test label", label_html: [class: "test-label-class"])
           |> safe_to_string()
 
         assert input =~ "Test label"
-        assert input =~ "test_class"
+        assert input =~ "test-label-class"
+        refute input =~ "label_html"
         ""
       end)
     end
@@ -32,11 +50,43 @@ defmodule PtrWeb.InputHelpersTest do
     test "with using option", %{conn: conn} do
       form_for(conn, "/", [as: :test], fn form ->
         input =
-          input(form, :name, "Test label", using: :password_input)
+          input(form, :name, nil, using: :password_input)
           |> safe_to_string()
 
         assert input =~ "type=\"password\""
         refute input =~ "using"
+        ""
+      end)
+    end
+
+    test "with icons option", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, nil, icons: [left: "envelope", right: "warning"])
+          |> safe_to_string()
+
+        assert input =~ "has-icons-left has-icons-right"
+        assert input =~ "is-left"
+        assert input =~ "is-right"
+        assert input =~ "fa-envelope"
+        assert input =~ "fa-warning"
+        refute input =~ "icons="
+        ""
+      end)
+    end
+
+    test "with icons left option", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, nil, icons: [left: "envelope"])
+          |> safe_to_string()
+
+        assert input =~ "has-icons-left"
+        refute input =~ "has-icons-right"
+        assert input =~ "is-left"
+        refute input =~ "is-right"
+        assert input =~ "fa-envelope"
+        refute input =~ "icons="
         ""
       end)
     end
