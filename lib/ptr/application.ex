51d01a023bc4ddef223b0b544cf70e15e939a2b6
Franco Catena <francocatena@gmail.com>
2017-09-29 15:10:31 -0300

    Make Ptr application more Elixir 1.5 =)

--- lib/ptr/application.ex
+++ lib/ptr/application.ex
@@ -4,21 +4,20 @@ defmodule Ptr.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-    import Supervisor.Spec
-
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(Ptr.Repo, []),
+      Ptr.Repo,
       # Start the endpoint when the application starts
-      supervisor(PtrWeb.Endpoint, []),
+      PtrWeb.Endpoint,
       # Start your own worker by calling: Ptr.Worker.start_link(arg1, arg2, arg3)
-      # worker(Ptr.Worker, [arg1, arg2, arg3]),
+      # {Ptr.Worker, [arg1, arg2, arg3]},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Ptr.Supervisor]
+
     Supervisor.start_link(children, opts)
   end
 
