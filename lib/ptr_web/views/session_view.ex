40c837d51c39f33bf4fa657f4ea50671010c4a8b
Franco Catena <francocatena@gmail.com>
2017-10-30 00:59:01 -0300

    Autocomplete off on production for session's email

--- lib/ptr_web/views/session_view.ex
+++ lib/ptr_web/views/session_view.ex
@@ -1,3 +1,19 @@
 defmodule PtrWeb.SessionView do
   use PtrWeb, :view
+
+  def email_input_html do
+    [
+      autofocus:    true,
+      autocomplete: autocomplete_email(),
+      class:        "input is-medium",
+      placeholder:  dgettext("sessions", "Email")
+    ]
+  end
+
+  defp autocomplete_email do
+    case Mix.env() do
+      "prod" -> "off"
+      _      -> "on"
+    end
+  end
 end
