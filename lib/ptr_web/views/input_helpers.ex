eff3764be0b1039ad1062b84d115a72eba05a0ca
Franco Catena <francocatena@gmail.com>
2017-11-14 11:30:13 -0300

    Input helper support select, textarea and addons

--- lib/ptr_web/views/input_helpers.ex
+++ lib/ptr_web/views/input_helpers.ex
@@ -4,17 +4,27 @@ defmodule PtrWeb.InputHelpers do
   import PtrWeb.ErrorHelpers, only: [error_tag: 2]
 
   def input(form, field, label \\ nil, opts \\ []) do
-    input_html = opts[:input_html] || []
-    label_html = opts[:label_html] || [class: "label"]
-    type       = opts[:using]      || input_type(form, field)
+    type       = opts[:using]      || input_type(form, field, opts)
     icon_opts  = opts[:icons]      || []
+    label_html = opts[:label_html] || [class: "label"]
+    addons     = opts[:addons]
+    input_opts = input_opts(type, opts)
     errors     = error_tag(form, field)
+    label      = label_for(form, field, label, label_html)
+    field      = content_tag(:div, class: field_class(opts)) do
+      input    = input(type, form, field, errors, icon_opts, input_opts, addons)
 
-    content_tag(:div, class: "field") do
-      label = label_for(form, field, label, label_html)
-      input = input(type, form, field, errors, icon_opts, input_html)
+      [addons(addons, :left), input, addons(addons, :right), errors]
+    end
 
-      [label, input, errors]
+    [label, field]
+  end
+
+  defp input_type(form, field, opts) do
+    if is_list(opts[:collection]) do
+      :select
+    else
+      input_type(form, field)
     end
   end
 
@@ -26,28 +36,63 @@ defmodule PtrWeb.InputHelpers do
     label(form, field, label, label_html)
   end
 
-  defp input(type, form, field, errors, icon_opts, input_html) do
-    input_opts   = input_opts(form, field, errors, input_html)
-    input        = apply(Phoenix.HTML.Form, type, [form, field, input_opts])
-    content_opts = [class: wrapper_classes(icon_opts)]
+  defp input(type, form, field, errors, icon_opts, opts, addons) do
+    input_opts   = input_opts(type, form, field, errors, opts)
+    input        = wrapped_input(type, form, field, errors, input_opts)
+    content_opts = [class: wrapper_classes(icon_opts, addons)]
 
     content_tag(:div, content_opts) do
       [input | input_icons(icon_opts)]
     end
   end
 
-  defp input_opts(form, field, errors, opts) do
+  defp addons(nil, _), do: ""
+  defp addons(addons, position) do
+    addon = addons[position]
+
+    if addon do
+      content_tag(:a, addon, class: "button is-static")
+    else
+      ""
+    end
+  end
+
+  defp input_opts(:select, opts) do
+    input_opts(nil, opts)
+    |> Keyword.put(:collection, opts[:collection] || [])
+  end
+
+  defp input_opts(_type, opts) do
+    opts = opts[:input_html] || []
+
+    opts |> Keyword.put(:addons, opts[:addons])
+  end
+
+  defp input_opts(type, form, field, errors, opts) do
     {class, opts} = Keyword.pop(opts, :class)
-    default_opts  = [class: input_class(class || "input", errors)]
-    validations   = input_validations(form, field)
+    default_opts  = [class: input_class(type, class, errors)]
+    validations   = input_validations(type, form, field)
 
     default_opts
     |> Keyword.merge(validations)
     |> Keyword.merge(opts)
   end
 
-  defp input_class(default, []), do: default
-  defp input_class(default, _),  do: "#{default} is-danger"
+  defp input_class(:textarea, nil, errors) do
+    input_class(:textarea, "textarea", errors)
+  end
+
+  defp input_class(type, nil, errors) when type != :select do
+    input_class(:input, "input", errors)
+  end
+
+  defp input_class(_, default, []), do: default
+  defp input_class(_, default, _),  do: "#{default} is-danger"
+
+  defp input_validations(:select, form, field) do
+    input_validations(form, field) |> Keyword.delete(:maxlength)
+  end
+  defp input_validations(_, form, field), do: input_validations(form, field)
 
   defp input_icons(icon_opts) do
     for {position, icon} <- icon_opts do
@@ -57,9 +102,38 @@ defmodule PtrWeb.InputHelpers do
     end
   end
 
-  defp wrapper_classes(icon_opts) do
+  defp field_class(opts) do
+    if is_list(opts[:addons]) do
+      "field has-addons"
+    else
+      "field"
+    end
+  end
+
+  defp wrapper_classes(icon_opts, addons) when is_list(addons) do
+    wrapper_classes(icon_opts, "control is-expanded")
+  end
+
+  defp wrapper_classes(icon_opts, nil) do
+    wrapper_classes(icon_opts, "control")
+  end
+
+  defp wrapper_classes(icon_opts, base_class) when is_binary(base_class) do
     icon_classes = for {position, _} <- icon_opts, do: "has-icons-#{position}"
 
-    ["control" | icon_classes] |> Enum.join(" ")
+    [base_class | icon_classes] |> Enum.join(" ")
+  end
+
+  defp wrapped_input(:select = type, form, field, errors, input_opts) do
+    {options, input_opts} = Keyword.pop(input_opts, :collection)
+    class = input_class(:input, "select is-fullwidth", errors)
+
+    content_tag(:div, class: class) do
+      apply(Phoenix.HTML.Form, type, [form, field, options, input_opts])
+    end
+  end
+
+  defp wrapped_input(type, form, field, _errors, input_opts) do
+    apply(Phoenix.HTML.Form, type, [form, field, input_opts])
   end
 end
--- test/ptr_web/views/input_helpers_test.exs
+++ test/ptr_web/views/input_helpers_test.exs
@@ -1,22 +1,25 @@
 defmodule PtrWeb.InputHelpersTest do
   use PtrWeb.ConnCase, async: true
 
+  import Phoenix.HTML, only: [html_escape: 1, safe_to_string: 1]
+
   describe "input" do
     import PtrWeb.InputHelpers
     import Phoenix.HTML.Form, only: [form_for: 4]
-    import Phoenix.HTML, only: [safe_to_string: 1]
 
     test "with default options", %{conn: conn} do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name)
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "Name"
         assert input =~ "class=\"field\""
         assert input =~ "class=\"control\""
         assert input =~ "class=\"input\""
         assert input =~ "class=\"label\""
+        refute input =~ "is-expanded"
         refute input =~ "has-icons"
         ""
       end)
@@ -26,9 +29,11 @@ defmodule PtrWeb.InputHelpersTest do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name, nil, input_html: [class: "test-input-class"])
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "test-input-class"
+        refute input =~ "is-expanded"
         refute input =~ "input_html"
         ""
       end)
@@ -38,10 +43,12 @@ defmodule PtrWeb.InputHelpersTest do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name, "Test label", label_html: [class: "test-label-class"])
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "Test label"
         assert input =~ "test-label-class"
+        refute input =~ "is-expanded"
         refute input =~ "label_html"
         ""
       end)
@@ -51,10 +58,43 @@ defmodule PtrWeb.InputHelpersTest do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name, nil, using: :password_input)
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "type=\"password\""
+        refute input =~ "is-expanded"
         refute input =~ "using"
+        ""
+      end)
+    end
+
+    test "with using textarea option", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, nil, using: :textarea)
+          |> input_to_string()
+
+        assert input =~ "<textarea"
+        assert input =~ "class=\"textarea\""
+        refute input =~ "is-expanded"
+        refute input =~ "using"
+        ""
+      end)
+    end
+
+    test "with collection option", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, nil, collection: [1, 2, 3])
+          |> input_to_string()
+
+        assert input =~ "<select"
+        assert input =~ "<div class=\"select is-fullwidth\">"
+        assert input =~ "<option value=\"1\">1</option>"
+        assert input =~ "<option value=\"2\">2</option>"
+        assert input =~ "<option value=\"3\">3</option>"
+        refute input =~ "is-expanded"
+        refute input =~ "collection"
         ""
       end)
     end
@@ -63,13 +103,15 @@ defmodule PtrWeb.InputHelpersTest do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name, nil, icons: [left: "envelope", right: "warning"])
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "has-icons-left has-icons-right"
         assert input =~ "is-left"
         assert input =~ "is-right"
         assert input =~ "fa-envelope"
         assert input =~ "fa-warning"
+        refute input =~ "is-expanded"
         refute input =~ "icons="
         ""
       end)
@@ -79,16 +121,40 @@ defmodule PtrWeb.InputHelpersTest do
       form_for(conn, "/", [as: :test], fn form ->
         input =
           input(form, :name, nil, icons: [left: "envelope"])
-          |> safe_to_string()
+          |> input_to_string()
 
+        assert input =~ "<input"
         assert input =~ "has-icons-left"
         refute input =~ "has-icons-right"
         assert input =~ "is-left"
         refute input =~ "is-right"
         assert input =~ "fa-envelope"
+        refute input =~ "is-expanded"
         refute input =~ "icons="
         ""
       end)
     end
+
+    test "with addons option", %{conn: conn} do
+      form_for(conn, "/", [as: :test], fn form ->
+        input =
+          input(form, :name, nil, addons: [left: "l$", right: "r$"])
+          |> input_to_string()
+
+        assert input =~ "<div class=\"field has-addons\">"
+        assert input =~ "<input"
+        assert input =~ "<a class=\"button is-static\">l$</a>"
+        assert input =~ "<a class=\"button is-static\">r$</a>"
+        assert input =~ "is-expanded"
+        refute input =~ "addons="
+        ""
+      end)
+    end
+  end
+
+  defp input_to_string(input) do
+    input
+    |> html_escape()
+    |> safe_to_string()
   end
 end
