f8c61fd210793864cfc18720fede339b484bad9f
Franco Catena <francocatena@gmail.com>
2017-11-14 11:27:47 -0300

    Trigger fake click only if element is not in link

--- assets/js/fake_link.js
+++ assets/js/fake_link.js
@@ -1,7 +1,8 @@
 document.addEventListener('click', event => {
   const linkLikeElement = event.target.closest('[data-link-like]')
+  const hasLinkParent   = event.target.closest('a')
 
-  if (linkLikeElement) {
+  if (linkLikeElement && !hasLinkParent) {
     const selector = linkLikeElement.dataset['target']
     const target   = linkLikeElement.querySelector(selector)
 
