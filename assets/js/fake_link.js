d569f7bf5c9a91c60e7923cab285f559136dbd81
Franco Catena <francocatena@gmail.com>
2017-12-03 18:23:07 -0300

    Only go to link if no parent link =)
    When checking a "fake link" do not follow if there is a real one as parent

--- assets/js/fake_link.js
+++ assets/js/fake_link.js
@@ -1,8 +1,8 @@
 document.addEventListener('click', event => {
   const linkLikeElement = event.target.closest('[data-link-like]')
-  const hasLinkParent   = event.target.closest('a')
+  const hasParentLink   = event.target.closest('a')
 
-  if (linkLikeElement && !hasLinkParent) {
+  if (linkLikeElement && !hasParentLink) {
     const selector = linkLikeElement.dataset['target']
     const target   = linkLikeElement.querySelector(selector)
 
